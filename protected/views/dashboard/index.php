f9bf39350a5e04a4a1cceda746a89aad9f8745ab
Hoeut <hoeut.heanh@gmail.com>
2019-08-29 18:22:18 +0700

    fixed html on pie chart

--- protected/views/dashboard/index.php
+++ protected/views/dashboard/index.php
@@ -1,58 +1,57 @@
 <?php
-$this->breadcrumbs=array(
-	Yii::t('app','Dashboard'),
+$this->breadcrumbs = array(
+    Yii::t('app', 'Dashboard'),
 );
 ?>
 <?php
-    $date = array();
-    $sub_total = array();
-    $total = array();
-    $purchase_aged = array();
-    $nclient = array();
+$date = array();
+$sub_total = array();
+$total = array();
+$purchase_aged = array();
+$nclient = array();
 
 
-    foreach($report->saleDailyChart() as $record)
-    {
-        $date[] = $record["date"];
-        $sub_total[] = $record["sub_total"];
-        $total[] = $record["total"];
-    }
+foreach ($report->saleDailyChart() as $record) {
+    $date[] = $record["date"];
+    $sub_total[] = $record["sub_total"];
+    $total[] = $record["total"];
+}
 
 
 
 ?>
 
 
-<div class="">
-        <div class="row">
+<div class="col-xs-12">
+    <div class="row">
 
-            <!--PAGE CONTENT BEGINS-->
-            <div class="col-xs-12">
+        <!--PAGE CONTENT BEGINS-->
+        <div class="col-xs-12">
 
-                <?php $this->renderPartial('partial/widget_agedpurchase', array(
-                    'report' => $report,
-                )); ?>
+            <?php $this->renderPartial('partial/widget_agedpurchase', array(
+                'report' => $report,
+            )); ?>
 
-                <div class="space-8"></div>
+            <div class="space-8"></div>
 
 
-               <?php $this->renderPartial('partial/widget_chart', array(
-                    'date' => $date,
-                    'sub_total' => $sub_total,
-                    'total' => $total,
-                )); ?>
+            <?php $this->renderPartial('partial/widget_chart', array(
+                'date' => $date,
+                'sub_total' => $sub_total,
+                'total' => $total,
+            )); ?>
 
-                <div class="space-8"></div>
+            <div class="space-8"></div>
 
-                <?php $this->renderPartial('partial/widget_topten', array(
-                    'report' => $report,
-                )); ?>
+            <?php $this->renderPartial('partial/widget_topten', array(
+                'report' => $report,
+            )); ?>
 
-            </div><!--/row-->
         </div>
+        <!--/row-->
+    </div>
 </div>
-          
-<a href="#" id="btn-scroll-up" class="btn-scroll-up btn btn-small btn-inverse">
-        <i class="icon-double-angle-up icon-only bigger-110"></i>
-</a>
 
+<a href="#" id="btn-scroll-up" class="btn-scroll-up btn btn-small btn-inverse">
+    <i class="icon-double-angle-up icon-only bigger-110"></i>
+</a>
---
+++ protected/views/dashboard/partial/widget_agedpurchase.php
@@ -0,0 +1,106 @@
+<div class="row">
+    <div class="col-xs-5 widget-container-col">
+        <div class="widget-box widget-color-blue2">
+			<div class="widget-header">
+				<h5 class="widget-title bigger lighter">
+					<i class="ace-icon fa fa-trophy"></i>
+					<?php echo Yii::t('app','Aged Customer Purchase') ?>
+				</h5>
+			</div>
+			<div class="widget-body">
+				<div class="widget-main">
+					<!-- #section:plugins/charts.flotchart -->
+					<div id="piechart-placeholder"></div>
+
+					<!-- /section:plugins/charts.flotchart -->
+					<div class="hr hr8 hr-double"></div>
+
+					<?php foreach ($report->dbAgedPurchase() as $value) {    ?>
+
+					<div class="clearfix">
+						<div class="grid3">
+							<span class="grey">
+								<i class="ace-icon fa fa-users fa-2x blue"></i>
+								<?= $value["aged_purchase"] ?>
+							</span>
+							<h4 class="bigger pull-right">
+								<a href="<?= Yii::app()->createUrl('report/agedcustomerpurchase', array('filter' => $value["ord"])); ?>"> 
+									<?= $value['nclient'] ?>        
+								</a>
+							</h4>
+						</div>      
+					</div>
+					<?php } ?>  
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<script type="text/javascript">
+    
+    var placeholder = $('#piechart-placeholder').css({'width':'90%' , 'min-height':'150px'});
+              var data = [
+                { label: "social networks",  data: 38.7, color: "#68BC31"},
+                { label: "search engines",  data: 24.5, color: "#2091CF"},
+                { label: "ad campaigns",  data: 8.2, color: "#AF4E96"},
+                { label: "direct traffic",  data: 18.6, color: "#DA5430"},
+                { label: "other",  data: 10, color: "#FEE074"}
+              ]
+              
+      function drawPieChart(placeholder, data, position) {
+          $.plot(placeholder, data, {
+            series: {
+                pie: {
+                    show: true,
+                    tilt:0.8,
+                    highlight: {
+                        opacity: 0.25
+                    },
+                    stroke: {
+                        color: '#fff',
+                        width: 2
+                    },
+                    startAngle: 2
+                }
+            },
+            legend: {
+                show: true,
+                position: position || "ne", 
+                labelBoxBorderColor: null,
+                margin:[-30,15]
+            }
+            ,
+            grid: {
+                hoverable: true,
+                clickable: true
+            }
+         })
+     }
+    
+    drawPieChart(placeholder, data);
+
+
+    placeholder.data('chart', data);
+    placeholder.data('draw', drawPieChart);
+
+    //pie chart tooltip example
+    var $tooltip = $("<div class='tooltip top in'><div class='tooltip-inner'></div></div>").hide().appendTo('body');
+    var previousPoint = null;
+
+    placeholder.on('plothover', function (event, pos, item) {
+        if(item) {
+            if (previousPoint != item.seriesIndex) {
+                previousPoint = item.seriesIndex;
+                var tip = item.series['label'] + " : " + item.series['percent']+'%';
+                $tooltip.show().children(0).text(tip);
+            }
+            $tooltip.css({top:pos.pageY + 10, left:pos.pageX + 10});
+        } else {
+            $tooltip.hide();
+            previousPoint = null;
+        }
+        
+    });
+
+</script>
